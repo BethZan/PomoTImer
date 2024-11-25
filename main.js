ffbace9e337fa5b5ae8436a117a9375cbc42d19a
BethZa <bethharv77@gmail.com>
2024-11-25 13:43:11 +0000

    Update 2 main.js

--- main.js
+++ main.js
@@ -3,25 +3,24 @@ var minutesE1 = document.getElementById(minutes)
 var secondsE1 = document.getElementById(seconds)
 
 function countdownTimer() {
-    const countDownDate = new Date('104/18/2024').getTime()
+    const countDownDate = new Date('24/11/2024').getTime()
     const second = 1000
     const minute = second * 60
     const hour = minute * 60
 
-const interval = setInterval(() => {
-
-}, 1000);
-
-   const now = new Date().getTime()
-   const distance = countDownDate - now
+    const interval = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = countDownDate - now
+        
         hoursE1.innertext = formatNumber(math.floor(distance / hour))
-    minutesE1.innertext = formatNumber(math.floor((distance % hour)/ minute)
-    )
-    seconds.innertext = formatNumber (math.floor((distance % minute)/ second))
+        minutesE1.innertext = formatNumber(math.floor((distance % hour) / minute))
+        seconds.innertext = formatNumber(math.floor((distance % minute) / second))
+    }, 1000);
+
 }
 
 function formatNumber(number) {
-    if(number < 10 ) {
+    if (number < 10) {
         return '0' + number
     }
 }
